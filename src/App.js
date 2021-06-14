7df5335a5f262b1c1cc7f7951b0a751f63fc35ba
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-06-15 01:07:44 +0600

    pass object to component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const products = [
+    {name: 'Photoshop', price: '$99.99'},
+    {name: 'Illustrator', price: '$69.99'},
+    {name: 'PDF Reader', price: '$29.99'},
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
+        <Products product={products[0]} ></Products>
+        <Products product={products[1]} ></Products>
+        <Products product={products[2]} ></Products>
         <Person name='Mohosin Palash' job='Student'></Person>
         <Person name='Shoptorshe Prova' job='Doctor'></Person>
       </header>
     </div>
   );
+}
+
+function Products(props){
+  const productStyle ={
+    border: '2px solid black',
+    borderRadius: '5px', 
+    backgroundColor: 'lightgray',
+    height: '220px',
+    width: '220px',
+    color: 'black',
+    float: 'left'
+  }
+  const {name, price} = props.product;
+  return (
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h2>{price}</h2>
+      <button>Buy Now</button>
+    </div>
+  )
 }
 
 function Person(props){
